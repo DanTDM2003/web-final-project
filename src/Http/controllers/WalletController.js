14e657e91d92cf1be9e9eca16dd646e54439097e
21127335 <sphlam21@clc.fitus.edu.vn>
2024-01-10 11:03:45 +0700

    fixing payment logic

--- src/Http/controllers/WalletController.js
+++ src/Http/controllers/WalletController.js
@@ -5,14 +5,14 @@ const Cart = require('../../models/Carts.js')
 module.exports = {
   update: async (req, res, next) => {
     try{
-      let user_id = req.body.id, firstName = req.body.firstName, lastName = req.body.lastName, email = req.body.email, phone = req.body.phone, address = req.body.address, cart = req.body.cart
-      await Payment.add({User_id: user_id, Firstname: firstName, Lastname: lastName, Email: email, Phone: phone, Address: address, Content: JSON.stringify(cart)})
       const balance = await Wallet.getCurrentBalance(req.body.id)
       const result = await Wallet.update(req.body.id, +req.body.total, balance.Balance);
       if(result == false){
         res.json({error: "Balance not enough!"})
       }
       else{
+        let user_id = req.body.id, firstName = req.body.firstName, lastName = req.body.lastName, email = req.body.email, phone = req.body.phone, address = req.body.address, cart = req.body.cart
+        await Payment.add({User_id: user_id, Firstname: firstName, Lastname: lastName, Email: email, Phone: phone, Address: address, Content: JSON.stringify(cart)})
         await Cart.resetCart(req.body.id)
         res.json({})
       }
