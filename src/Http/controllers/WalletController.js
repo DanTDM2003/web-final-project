f514d1d26d70bc218a0c3ac3661e3eb37a49a295
21127335 <sphlam21@clc.fitus.edu.vn>
2024-01-10 15:03:59 +0700

    Implement sales statistic (sales per month)

--- src/Http/controllers/WalletController.js
+++ src/Http/controllers/WalletController.js
@@ -11,8 +11,11 @@ module.exports = {
         res.json({error: "Balance not enough!"})
       }
       else{
-        let user_id = req.body.id, firstName = req.body.firstName, lastName = req.body.lastName, email = req.body.email, phone = req.body.phone, address = req.body.address, cart = req.body.cart
-        await Payment.add({User_id: user_id, Firstname: firstName, Lastname: lastName, Email: email, Phone: phone, Address: address, Content: JSON.stringify(cart)})
+        let dateObject = new Date();
+        dateObject.setHours(dateObject.getHours() + 7);
+        let user_id = req.body.id, firstName = req.body.firstName, lastName = req.body.lastName, email = req.body.email, phone = req.body.phone, address = req.body.address, cart = req.body.cart;
+        let date = dateObject.toISOString().replace(/T/, ' ').replace(/\..+/, '')
+        await Payment.add({User_id: user_id, Firstname: firstName, Lastname: lastName, Email: email, Phone: phone, Address: address, Content: JSON.stringify(cart), Date: date})
         await Cart.resetCart(req.body.id)
         res.json({})
       }
